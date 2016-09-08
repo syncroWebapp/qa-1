f5b82fcd34861aa65412b62c3ae75bab8ae481dc
syncroWebapp <syncroWebapp@users.noreply.github.com>
2016-09-08 15:45:59 +0300

    Update publish.sh

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,23 +1,30 @@
-echo "Cleaning output..."
-rm -rf dita-ot*
-rm -rf oxygen-webhelp*
-rm -rf com.oxygenxml*
-rm -rf oxygen_custom*
-rm -rf out
-
-echo "Downloading ZIP files..."
 wget http://mirror.oxygenxml.com/InstData/Editor/Webhelp/oxygen-webhelp.zip
-wget https://github.com/dita-ot/dita-ot/releases/download/2.3.2/dita-ot-2.3.2.zip
 unzip oxygen-webhelp.zip 
+
+wget https://github.com/dita-ot/dita-ot/releases/download/2.3.2/dita-ot-2.3.2.zip
 unzip dita-ot-2.3.2.zip 
 
 cp -R com.oxygenxml.* dita-ot-2.3.2/plugins/
 mv dita-ot-2.3.2/plugins/com.oxygenxml.webhelp/plugin_2.x.xml dita-ot-2.3.2/plugins/com.oxygenxml.webhelp/plugin.xml
+# Add the editlink plugin
+git clone https://github.com/ctalau/ditaot-editlink-plugin dita-ot-2.3.2/plugins/com.oxygenxml.editlink/
 
 cd dita-ot-2.3.2/
 bin/ant -f integrator.xml 
 cd ..
 
 cp licenseKey.txt dita-ot-2.3.2/plugins/com.oxygenxml.webhelp/licensekey.txt
+
+rm -rf out
+
+REPONAME=`basename $PWD`
+PARENTDIR=`dirname $PWD`
+USERNAME=`basename $PARENTDIR`
+
+# Send some parameters to the "editlink" plugin as system properties
+export ANT_OPTS="$ANT_OPTS -Dditamap.path=$DITAMAP"
+export ANT_OPTS="$ANT_OPTS -Dcwd=`pwd`"
+export ANT_OPTS="$ANT_OPTS -Drepo.url=github://getFileContent/$USERNAME/$REPONAME/$TRAVIS_BRANCH/"
+export ANT_OPTS="$ANT_OPTS -Dwebapp.url=https://www.oxygenxml.com/webapp-demo-aws/"
 
 dita-ot-2.3.2/bin/dita -i $DITAMAP -f webhelp-responsive -filter=$DITAVAL
